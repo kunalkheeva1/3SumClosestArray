ac7f615c072537e8c3932ff92dc3dec1c329a51f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 23:14:13 -0700

    method to get the triplets whose combined sum is less than given sum

--- src/ThreeSumClosestArray.java
+++ src/ThreeSumClosestArray.java
@@ -1,3 +1,28 @@
+import java.util.Arrays;
+
 public class ThreeSumClosestArray {
-    
+
+    public static int threeSumClosestArray(int arr[], int n, int sum){
+
+        Arrays.sort(arr);
+        int i, j, k, count= 0;
+
+        for( i= 0; i<n; i++){
+            j = i+1;
+            k= n-1;
+            while(j<k){
+                if(arr[i]+ arr[j]+ arr[k]< sum){
+                    count += k-j;
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }return count;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
