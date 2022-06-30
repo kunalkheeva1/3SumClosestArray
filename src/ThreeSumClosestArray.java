6e8c328b4188470f40371446f47a08cb1114334b
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 23:16:59 -0700

    added comments for better references, although i still dont know how the count is being updated in while loop, but will ask yash tomorrow

--- src/ThreeSumClosestArray.java
+++ src/ThreeSumClosestArray.java
@@ -3,18 +3,21 @@ import java.util.Arrays;
 public class ThreeSumClosestArray {
 
     public static int threeSumClosestArray(int arr[], int n, int sum){
-
+    //sort it first
         Arrays.sort(arr);
         int i, j, k, count= 0;
-
+        //start pointers from first second and last position
         for( i= 0; i<n; i++){
             j = i+1;
             k= n-1;
+            // until the second point meets last
             while(j<k){
+                //if its smaller then increase the count and j
                 if(arr[i]+ arr[j]+ arr[k]< sum){
                     count += k-j;
                     j++;
                 }
+                //else look for smaller sum by decrementing the index of last pointer
                 else {
                     k--;
                 }
